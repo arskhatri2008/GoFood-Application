8f0a836238fa5bb54147f055fdeea4aaffb4fb7b
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-01-26 23:07:31 +0500

    continue

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
         <div><Navbar/></div>
+        <div><Footer/></div>
     </div>
   )
 }
