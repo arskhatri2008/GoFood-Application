5e32a944c88c41af289626b87661bf832f938ad3
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-01-30 23:02:28 +0500

    continue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 // import logo from './logo.svg';
 import './App.css';
 import Home from './screens/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './screens/Login';
 
 function App() {
   return (
-    <>
-    <div><Home/></div>
-    </>
+    <Router>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+    </Router>
   );
 }
 
